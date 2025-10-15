8c6328d653b8d02c1b140488e09046e8f275c0d2
Rhissi <hissi.ryan@gmail.com>
2025-10-16 01:35:47 +0200

    Rotation script
    I added a script for the rotation, probably need to work more on it but it's the main idea of the use.

--- _Horlogement_Unity/Assets/Scripts/GearRotation.cs
+++ _Horlogement_Unity/Assets/Scripts/GearRotation.cs
@@ -1,16 +1,12 @@
+using System;
 using UnityEngine;
 
 public class GearRotation : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 5f;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        transform.Rotate(new Vector3(0, 0, rotationSpeed) * Time.deltaTime); // Just doing a rotation in the z axis.
     }
 }
