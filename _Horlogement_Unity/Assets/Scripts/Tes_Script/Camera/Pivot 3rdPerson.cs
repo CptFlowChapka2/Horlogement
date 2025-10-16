0d9b8bd85207f7726ded6f5c3cd4a737d52b50eb
CptFlowChapka2 <eterrien5@myges.fr>
2025-10-16 19:11:02 +0200

    3rd Cam done
    -Added bool to choose between the two method
    
    -Legacy : the proper (but buggy) solution
    -BlackMagick : a godamm hack accesing transfrom.forward directly

--- _Horlogement_Unity/Assets/Scripts/Tes_Script/Camera/Pivot 3rdPerson.cs
+++ _Horlogement_Unity/Assets/Scripts/Tes_Script/Camera/Pivot 3rdPerson.cs
@@ -8,29 +8,51 @@ public class Pivot3rdPerson : MonoBehaviour
 
     private InputAction cameraAction; //Datatype for all action in NewInputSystem
     private Vector3 cameraMouvement;
-    public float cameraSpeed = 1f;
+    public float cameraSpeedX = 1f;
+    public float cameraSpeedY = 1f;
+
+    public float clampMin = -0.99f;//must be negative and above -1
+    public float clampMax = 0.99f;//must be positive and below 1
+
+    public bool blackMagick = true;
+
 
     private void Start()
     {
-        cameraAction = InputSystem.actions.FindAction("Look");//we assigne this action kinda like get.Component
+        cameraAction = InputSystem.actions.FindAction("Look"); //we assigne this action kinda like get.Component
     }
     private void Update()
     {
-        // cameraMouvement = cameraAction.ReadValue<Vector2>();//we recover teh V2 value of the action
-        // transform.position = player.transform.position;//we maintain the pivot on the player
-        //
-        // Vector3 currentEuleur = transform.eulerAngles;//we remember the Initial angles
-        //
-        // float currentEuleurX = currentEuleur.x + cameraMouvement.y * cameraSpeed * Time.deltaTime;//we add the mouse mouvement value in the correct angles
-        // float currentEuleurY = currentEuleur.y - cameraMouvement.x * cameraSpeed * Time.deltaTime;
-        //
-        // currentEuleur.x = currentEuleurX;//we assign the new values
-        //
-        // currentEuleur.y = currentEuleurY;
-        // transform.eulerAngles = currentEuleur;//we update the angles
-        
-        
-        
-       
+        cameraMouvement = cameraAction.ReadValue<Vector2>(); //we recover teh V2 value of the action
+        transform.position = player.transform.position; //we maintain the pivot on the player
+        Vector3 currentEuleur = transform.eulerAngles; //we remember the Initial angles
+
+        if (blackMagick)//Evil ass solution but smooth as all hell 
+        {
+            //we calculate Horizontal Mouvement First
+            float currentEuleurY = currentEuleur.y + cameraMouvement.x * cameraSpeedX * Time.deltaTime;
+            currentEuleur.y = currentEuleurY;
+            transform.eulerAngles = currentEuleur;//applying the rotation
+            
+            //Calulate Vertical Mouvement
+            //We take the Normalized vector pointing in the local forward and directly modify it
+            transform.forward = new Vector3(transform.forward.x //keeping the same X and Z since  that taken care of Above
+                //evil fucking magick : transform.forward is always normalised meaning 1 is 90° and -1 is -90° (for our usage)
+                //this if we add on Y axis we point upper or downer ,as long as the value is between 0&1 insured by low CamSpeedY and DeltaTime
+                , Math.Clamp(transform.forward.y + cameraMouvement.y * cameraSpeedY * Time.deltaTime, clampMin, clampMax)//we Clamp this value before 1 to avoid Jitterness
+                , transform.forward.z);
+        }
+        else //"proper" solution but you can't clamp euleurAngles (cuz Unity LIES) so weird as all hell when looking up or down
+        {
+            float currentEuleurX = currentEuleur.x + cameraMouvement.y * cameraSpeedX * Time.deltaTime; //we add the mouse mouvement value in the correct angles
+            float currentEuleurY = currentEuleur.y - cameraMouvement.x * cameraSpeedX * Time.deltaTime;
+
+            currentEuleur.x = currentEuleurX; //we assign the new values
+
+            currentEuleur.y = currentEuleurY;
+            transform.eulerAngles = currentEuleur; //we update the angles
+        }
+
+
     }
 }
