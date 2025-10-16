f3b0891a528087600e121c7ac0e5166df28ce00b
CptFlowChapka2 <eterrien5@myges.fr>
2025-10-16 19:30:33 +0200

    Wrote in the wrong Script
    -i'm dumb

--- _Horlogement_Unity/Assets/Scripts/Tes_Script/Camera/CameraAnchorCam.cs
+++ _Horlogement_Unity/Assets/Scripts/Tes_Script/Camera/CameraAnchorCam.cs
@@ -2,5 +2,10 @@ using UnityEngine;
 
 public class CameraAnchorCam : MonoBehaviour
 {
-   
+    public GameObject target;
+
+    private void Update()
+    {
+        transform.LookAt(target.transform.position,Vector3.up);
+    }
 }
--- _Horlogement_Unity/Assets/Scripts/Tes_Script/Camera/Pivot_AnchorCam.cs
+++ _Horlogement_Unity/Assets/Scripts/Tes_Script/Camera/Pivot_AnchorCam.cs
@@ -3,10 +3,5 @@ using UnityEngine;
 
 public class Pivot_AnchorCam : MonoBehaviour
 {
-    public GameObject target;
-
-    private void Update()
-    {
-        transform.LookAt(target.transform.position,Vector3.up);
-    }
+    
 }
