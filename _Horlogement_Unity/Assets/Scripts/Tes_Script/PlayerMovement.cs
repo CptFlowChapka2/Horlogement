0b3bfbb231a8951549e380801e120879a258e9b7
CptFlowChapka2 <eterrien5@myges.fr>
2025-10-17 17:10:21 +0200

    Testing arround Ryan's Cam
    +reoder multiplication for optimisation sake

--- _Horlogement_Unity/Assets/Scripts/Tes_Script/PlayerMovement.cs
+++ _Horlogement_Unity/Assets/Scripts/Tes_Script/PlayerMovement.cs
@@ -22,6 +22,6 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveDirection * speed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + moveDirection * (speed * Time.fixedDeltaTime));
     }
 }
