bcc68c8be43ad29a1a925a0e05fbcbea4a389e7a
CptFlowChapka2 <eterrien5@myges.fr>
2025-10-22 23:15:30 +0200

    -Added Gear Break and Action
    -Fully commented  script for both

--- Assets/Gears/Script/GearAction.cs
+++ Assets/Gears/Script/GearAction.cs
@@ -2,15 +2,36 @@ using UnityEngine;
 
 public class GearAction : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    //method to add a speedChange to an Gear //todo:Verify player direction relative to gear.
+    public void TransferRotation(GameObject targetGear,float change) 
     {
-        
+        GearPhysRotation targetRotation;
+        if (targetGear.TryGetComponent<GearPhysRotation>(out targetRotation))
+        {
+            targetRotation.rotationSpeed += change;
+        }
+    }
+    //Call A ForcedBreak event on that Gear imediatly trigger it's local Break
+    public void ForceBreak(GameObject targetGear)
+    {
+        GearBreak targetBreak;
+        if (targetGear.TryGetComponent<GearBreak>(out targetBreak))
+        {
+           targetBreak.ForceBreak();
+        }
+
+    }
+    //Toggle ON/off the  possibility of a Break for a Gear
+    public void ToggleBreak(GameObject targetGear,bool mayhaps)
+    {
+        GearBreak targetBreak;
+        if (targetGear.TryGetComponent<GearBreak>(out targetBreak))
+        {
+            targetBreak.canBreak=mayhaps;
+        }
+    
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+
 }
--- Assets/Gears/Script/GearBreak.cs
+++ Assets/Gears/Script/GearBreak.cs
@@ -1,16 +1,164 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GearBreak : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float speedDecrease;//MUST BE POSSITIVE. Value added or substracted to the Gear
+    [SerializeField] private mode mode; //See Enum Mode Below .
+
+    [Header("Parameters")] 
+    
+    //parameter for random mode
+    [SerializeField]private float random_RangeMin = 5f;
+    [SerializeField] private float random_RangeMax = 30f;//the min and max for the Random.range
+    [SerializeField] private bool random_reRandomOnBreak;//if the Random intervall must be reroll on each Break
+    
+    [SerializeField] private bool fullStop;//when true set gear.rotationSpeed to 0
+    [SerializeField] private float delay_delay = 5f;//the delay for the delay mode .delay
+
+    private float cooldown;
+    private float initialCooldown;
+
+    private GearPhysRotation thisRotation;
+
+    public bool canBreak;//if the Break behavior work
+
+    private void Start()
     {
+        gameObject.TryGetComponent<GearPhysRotation>(out thisRotation);//recover this gameObject GearRotation
         
+        
+
+        if (mode == mode.Random) // On random mode will trigger a Break event after a fixed* ammount of time
+        {
+            cooldown = Random.Range(random_RangeMin, random_RangeMax);
+
+
+        }
+        else if (mode == mode.Delay)//will trigger a Break after a fixed Delay
+        {
+            cooldown = delay_delay;
+
+
+        }
+        else //the Continius mode , reduce speed every frame
+        {
+            cooldown = 0f;
+        }
+
+        initialCooldown = cooldown;//cache in the initial delay
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (canBreak)
+        {
+            cooldown -= Time.deltaTime;
+            Break();
+        }
+
+
     }
+
+    private void Break()
+    {
+        //Great Wall of If // all of them reset cooldown inside & check for rotationspeed to know if you add or substract
+        //todo: use switch case
+        if (mode == mode.Random)
+        {
+            if (cooldown <= 0f)
+            {
+                if (random_reRandomOnBreak)
+                {
+                    cooldown = Random.Range(random_RangeMin, random_RangeMax);
+                }
+                else
+                {
+                    cooldown = initialCooldown;
+                }
+
+                if (fullStop)
+                {
+                    thisRotation.rotationSpeed = 0f;
+                }
+                else
+                {
+
+
+                    if (thisRotation.rotationSpeed > 0)
+                    {
+                        thisRotation.rotationSpeed -= speedDecrease;
+                    }
+                    else
+                    {
+                        thisRotation.rotationSpeed += speedDecrease;
+
+                    }
+                }
+
+
+            }
+
+
+        }
+        else if (mode == mode.Delay)
+        {
+            if (cooldown <= 0f)
+            {
+                cooldown = initialCooldown;
+                if (fullStop)
+                {
+
+                    thisRotation.rotationSpeed = 0f;
+                }
+                else
+                {
+
+                    if (thisRotation.rotationSpeed > 0)
+                    {
+                        thisRotation.rotationSpeed -= speedDecrease;
+                    }
+                    else
+                    {
+                        thisRotation.rotationSpeed += speedDecrease;
+
+                    }
+                }
+            }
+
+
+        }
+        else if (mode == mode.Continius)
+        {
+            if (fullStop)
+            {
+                thisRotation.rotationSpeed = 0f;
+            }
+            else
+            {
+                if (thisRotation.rotationSpeed > 0)
+                {
+                    thisRotation.rotationSpeed -= speedDecrease;
+                }
+
+                if (thisRotation.rotationSpeed < 0)
+                {
+                    thisRotation.rotationSpeed += speedDecrease;
+                }
+            }
+
+        }
+    }
+    public void ForceBreak()//WRATH OF ZEUS !!
+    {Break();}
+
+
+}
+
+enum mode //make the little multichoice button in editor feel nice 
+{
+    Random,
+    Continius,
+    Delay,
 }
