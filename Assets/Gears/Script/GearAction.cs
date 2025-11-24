aaa4d467a98401572713da35ecb51fe043b012d5
CptFlowChapka2 <eterrien5@myges.fr>
2025-11-24 21:52:15 +0100

    Reactive TransfertSpeed done

--- Assets/Gears/Script/GearAction.cs
+++ Assets/Gears/Script/GearAction.cs
@@ -4,12 +4,26 @@ public class GearAction : MonoBehaviour
 {
     [SerializeField] public float transfertInitialSpeed = 1f;
     [SerializeField] public float transfertSustainSpeed = 1f;
-    public void TransferRotation(GameObject targetGear,float change,float speedModifier) 
+    [SerializeField] public float AngleOfAcceptance = 45f;
+    public void TransferRotation(GameObject targetGear,GameObject player,Vector3 mouv,float speedModifier) 
     {
         GearPhysRotation targetRotation;
         if (targetGear.TryGetComponent<GearPhysRotation>(out targetRotation))
         {
-            targetRotation.rotationSpeed += change*speedModifier;
+            Vector3 gearToPlayer = player.transform.position-targetGear.transform.position;
+            Vector3 perpendicularPositive = Vector3.ProjectOnPlane(Quaternion.AngleAxis(90f,Vector3.up)*gearToPlayer,Vector3.up);
+            float angle =Vector3.Angle(mouv,perpendicularPositive) ;
+            if (angle <= 45f)
+            {
+                targetRotation.rotationSpeed += speedModifier;
+            }
+            else if (angle>=90f)
+            {
+                targetRotation.rotationSpeed += -speedModifier;
+            }
+
+
+
         }
     }
     //Call A ForcedBreak event on that Gear imediatly trigger it's local Break
