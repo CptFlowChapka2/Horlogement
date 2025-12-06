167d987b7f6fd4d6e1f0cd26d96a9dd22ed8b084
CptFlowChapka2 <eterrien5@myges.fr>
2025-12-06 15:04:43 +0100

    Ortho Cam

--- Assets/Grid/GridManager.cs
+++ Assets/Grid/GridManager.cs
@@ -43,6 +43,7 @@ public class GridManager : MonoBehaviour
        allUpBorder.ForEach(x=>wallCreator.CreateWall(x,allUpBorder.Find(a=>a.coords.x.Equals(x.coords.x+1))));
        List<Tile> allDownBorder = allTile.FindAll(x => x.coords.y.Equals(0));
        allDownBorder.ForEach(x=>wallCreator.CreateWall(x,allDownBorder.Find(a=>a.coords.x.Equals(x.coords.x+1))));
+       
        List<Tile> allRightBorder = allTile.FindAll(x => x.coords.x.Equals(gridSize.x-1));
        allRightBorder.ForEach(x=>wallCreator.CreateWall(x,allRightBorder.Find(a=>a.coords.y.Equals(x.coords.y+1))));
        List<Tile> allLeftBorder = allTile.FindAll(x => x.coords.x.Equals(0));
