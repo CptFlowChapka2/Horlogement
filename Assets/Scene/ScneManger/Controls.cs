c4dfcdf815a051ea869943b40ae1e825511a659d
CptFlowChapka2 <eterrien5@myges.fr>
2026-01-28 02:47:48 +0100

    nkjl

--- Assets/Scene/ScneManger/Controls.cs
+++ Assets/Scene/ScneManger/Controls.cs
@@ -5,12 +5,32 @@ using UnityEngine;
 
 public class Controls : MonoBehaviour
 {
-   public List<TextMeshProUGUI> textMesh;
+   public List<SpriteRenderer> textMesh;
+
+   private SpriteRenderer spriteRenderer;
+   public Sprite sprite1;
+   public Sprite sprite2;
+   public Sprite sprite3;
 
 
+   private void Start()
+   {
+      spriteRenderer = GetComponent<SpriteRenderer>();
+   }
 
+   
+   private void OnMouseEnter()
+   {
+      spriteRenderer.sprite = sprite2;
+   }
+
+   private void OnMouseExit()
+   {
+      spriteRenderer.sprite = sprite1;
+   }
    private void OnMouseUpAsButton()
    {
+      spriteRenderer.sprite = sprite3;
       textMesh.ForEach(x=>x.enabled=!x.enabled);
    }
 }
--- Assets/Scene/ScneManger/titleButtonScript.cs
+++ Assets/Scene/ScneManger/titleButtonScript.cs
@@ -1,4 +1,5 @@
 
+using System;
 using UnityEngine;
 
 public class titleButtonScript : MonoBehaviour
@@ -6,11 +7,17 @@ public class titleButtonScript : MonoBehaviour
     public bool isButton = false;
 
     private SceneMangerScript sceneMangerScript;
+    private SpriteRenderer spriteRenderer;
+
+    public Sprite sprite1;
+    public Sprite sprite2;
+    public Sprite sprite3;
 
     public titleButton sceneTo;
 
     private void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
         sceneMangerScript = FindAnyObjectByType<SceneMangerScript>();
     }
 
@@ -18,7 +25,18 @@ public class titleButtonScript : MonoBehaviour
     private void OnMouseUpAsButton()
     {
         if(!isButton)return;
+        spriteRenderer.sprite = sprite3;
         sceneMangerScript.Load(sceneTo);
+    }
+
+    private void OnMouseEnter()
+    {
+        spriteRenderer.sprite = sprite2;
+    }
+
+    private void OnMouseExit()
+    {
+        spriteRenderer.sprite = sprite1;
     }
 
     private void OnTriggerEnter(Collider other)
